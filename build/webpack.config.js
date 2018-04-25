ce331a298daa2fe80c361c41150c7828e3fb7f89
Peter Zhang <peter.zhang.henan@gmail.com>
2018-04-25 19:08:40 +0800

    vue init fuck

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,3 +1,49 @@
 /**
  * Created by peterzhang on 2018/4/23.
  */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { VueLoaderPlugin } = require('vue-loader')
+
+const config = {
+    entry: path.resolve(__dirname, '../examples/main.js'),
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, '../dist'),
+    },
+    resolve: {
+        extensions: ['.js', '.vue'],
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js'
+        }
+    },
+    module: {
+        rules: [{
+            test: /\.vue$/,
+            loader: 'vue-loader'
+        }, {
+            test: /\.js$/,
+            exclude: /(node_modules|bower_components)/,
+            loader: 'babel-loader'
+        }, {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+        }, {
+            test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+            loader: 'url-loader',
+            options: {
+                limit: 10000
+            }
+        }]
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, '../examples/index.html')
+        })
+    ],
+    mode: 'development'
+
+};
+
+module.exports = config;
---
+++ examples/main.js
@@ -0,0 +1,8 @@
+/**
+ * Created by peterzhang on 2018/4/23.
+ */
+import './example.css'
+import Vue from 'vue'
+import App from './App.vue'
+
+console.log('ab')
