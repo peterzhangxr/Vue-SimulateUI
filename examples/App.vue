36864c29bb904e5c59fd5b7a60238673c94cb5f6
Peter Zhang <peter.zhang.henan@gmail.com>
2018-04-26 16:45:33 +0800

    app

--- examples/App.vue
+++ examples/App.vue
@@ -3,6 +3,7 @@
         <s-switch v-model="tile"></s-switch>{{tile}}
         <s-avatar url="https://placehold.it/144x144" :tile="tile"></s-avatar>
         <s-btn color="primary" block loading>
+
             <!--<s-icon name="map"></s-icon> 按钮-->
             加载中...
         </s-btn>
